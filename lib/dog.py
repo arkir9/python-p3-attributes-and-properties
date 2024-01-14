5d26441507b8981d3a77f60989a16bab8968d904
Mohammed <khamismitch@gmail.com>
2024-01-14 15:33:15 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +10,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="DefaultName", breed=None):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            return
+
+        if breed is not None and breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return
+
+        self.name = name
+        self.breed = breed
+
+# Example usage:
+fido = Dog(name="Fido", breed="Pug")
